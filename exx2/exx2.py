e6540242923367ce0a1b3fef576cc0aa84aefdfb
Jose Rhawan Michel Dos Santos Beltrão <rhawanjs@gmail.com>
2025-06-16 15:44:28 -0300

    segundo exercicio

---
+++ exx2/exx2.py
@@ -0,0 +1,5 @@
+dia = int(input("digite o dia que nasceu"))
+Mes = input("digite o mes")
+Ano = int(input("digite o ano que voce nasceu"))
+
+print(f"Voce nasceu no {dia} de {Mes} de {Ano}. correto?")
